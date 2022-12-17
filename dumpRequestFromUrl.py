ae949fe3fa713d5a5dcac0d48f9e0de616d078dd
Berkay Karatay <65409456+KarateB@users.noreply.github.com>
2022-12-17 14:04:58 +0300

    request added with data imported

--- dumpRequestFromUrl.py
+++ dumpRequestFromUrl.py
@@ -5,7 +5,40 @@ import json
 get_bin_numbers = request(method='Get', url='https://ppgpayment-test.birlesikodeme.com:20000/api/ppg/Payment/BinList')
 request_json = get_bin_numbers.json()
 request_result = request_json['result']
-real_data = json.dumps(request_result, indent=2, ensure_ascii=False)
-real_data = real_data.strip(' ] ').strip('[ ')
+real_data = json.dumps(request_result, ensure_ascii=False)
+real_data = real_data.strip(' ] ').strip('[ ').split('},')
+data_list = dict()
+a = 1
 
-print(real_data)
+for _ in real_data:
+    object_string = _.replace('{', '').replace('}', '')
+    object_string = object_string.replace('"', "").replace("'", "").split(',')
+    split_list = list()
+    prefix_name = ''
+    for _ in object_string:
+        item_key = _.split(': ')[0]
+        item_value = _.split(': ')[1]
+        if 'prefixno' in item_key.lower():
+            prefix_name = item_value
+        if str(item_value).replace("'", "") == 'false':
+            item_value = False
+        if str(item_value).replace("'", "") == 'true':
+            item_value = True
+
+        split_list.append([item_key, item_value])
+
+    data_list[prefix_name] = list(split_list)
+    a += 1
+
+load_dict = dict()
+inner_dict = dict()
+
+for a in data_list:
+    inner_dict = dict()
+    for b in data_list[a]:
+        inner_dict[str(b[0])] = b[1]
+    load_dict[str(a)] = inner_dict
+
+for _ in sorted(load_dict):
+    print(_, load_dict[_])
+
