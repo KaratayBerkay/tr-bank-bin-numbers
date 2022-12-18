2c98bb4db756a9e29601406a694930b926de2749
Berkay Karatay <65409456+KarateB@users.noreply.github.com>
2022-12-18 15:52:49 +0300

    Code cleaned

--- dumpRequestFromUrl.py
+++ dumpRequestFromUrl.py
@@ -1,14 +1,6 @@
 from requests import request
 import json
 
-url = 'https://ppgpayment-test.birlesikodeme.com:20000/api/ppg/Payment/BinList'
-get_bin_numbers = request(method='Get', url=url)
-request_json = get_bin_numbers.json()
-request_result = request_json['result']
-real_data = json.dumps(request_result, ensure_ascii=False)
-real_data = real_data.strip(' ] ').strip('[ ').split('},')
-data_list = dict()
-
 
 class BinNumbers:
     isBusinessCard: bool
@@ -53,8 +45,11 @@ class BinNumbers:
         return bin_numbers
 
 
-def seperate_dictionary():
+def seperate_dictionary(real_data):
     a = 1
+    data_list = dict()
+    load_dict = dict()
+
     for _ in real_data:
         object_string = _.replace('{', '').replace('}', '')
         object_string = object_string.replace('"', "").replace("'", "").split(',')
@@ -75,8 +70,6 @@ def seperate_dictionary():
         data_list[prefix_name] = list(split_list)
         a += 1
 
-    load_dict = dict()
-
     for a in data_list:
         inner_dict = dict()
         for b in data_list[a]:
@@ -95,21 +88,29 @@ def get_bank_list(bin_number_list):
     return bank_list
 
 
-load_dict = seperate_dictionary()   # Get whole dictionary into load object
-bin_numbers = BinNumbers(load_dict=load_dict)   # Get BinNumbers into numbers
+def all_bin_numbers_to_list(load_dict):
+    bin_number_list = []
+    for _ in load_dict:
+        bin_number = bin_numbers.find_bin_number(_)
+        bin_number_list.append(bin_number)
+    return bin_number_list
 
-bin_number_list = []   # Append items into list then loop over
 
-for _ in load_dict:
-    bin_number = bin_numbers.find_bin_number(_)
-    bin_number_list.append(bin_number)
+url = 'https://ppgpayment-test.birlesikodeme.com:20000/api/ppg/Payment/BinList'
+get_bin_numbers = request(method='Get', url=url)   # get bin numbers from url request
+request_json = get_bin_numbers.json()  # Get .json result
+request_result = request_json['result']    # Get result of .json only
+realm_data = json.dumps(request_result, ensure_ascii=False)   # Check for Turkish charset in keyboard
+realm_data = realm_data.strip(' ] ').strip('[ ').split('},')  # Split data string to listable object
 
-bank_list = get_bank_list(bin_number_list)      # Get bank_list
 
-for _ in bin_number_list:   # print items in list
-    print(_)
+loaded_dict = seperate_dictionary(real_data=realm_data)   # Get whole dictionary into load object
+bin_numbers = BinNumbers(load_dict=loaded_dict)   # Get BinNumbers into numbers
+bin_number_list_all = all_bin_numbers_to_list(load_dict=loaded_dict)  # Append items into list then loop over
 
-print(len(bank_list))
-for _ in bank_list:
-    print(_)
+bank_list = get_bank_list(bin_number_list_all)      # Get bank_list
+
+a_list = list(print(_) for _ in bin_number_list_all)    # print items in Bin Number list
+b_list = list(print(_) for _ in bank_list)    # print items in Bank list
+print('Length of banks :', len(bank_list))   # Total Bank Count
 
